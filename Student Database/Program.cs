cf9c7471632abd7d557b857fbaaf326e9a8ba782
dyandilthusha <ryandilthusha@gmail.com>
2023-09-11 03:06:41 +0530

    Add project files.

---
+++ Student Database/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace SMS_JUN_JULY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How Many Students are there ? ");
+            int scount = int.Parse(Console.ReadLine());
+
+            int i = 1;
+            while (i <= scount)
+            {
+                Console.WriteLine(" Student " + i + " Information ");
+
+                Console.WriteLine("Enter Name : ");
+                String studentname = Console.ReadLine();
+
+                Console.WriteLine("Enter Batch Name : ");
+                String studentbatch = Console.ReadLine();
+
+                Console.WriteLine("Enter Your DOB : ");
+                String studentdb = Console.ReadLine();
+
+                Console.WriteLine("Enter Your Gender : ");
+                String studentgender = Console.ReadLine();
+
+
+                //create student object=> classname objectname = new classname();
+                Student std = new Student(studentname, studentbatch, studentdb, studentgender);
+
+                //lets display both students details
+                std.display_details();
+                i++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ Student Database/Student.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace SMS_JUN_JULY
+{
+    class Student
+    {
+        public String sname;
+        public String batchname;
+        public String dob;
+        public String gender;
+         
+
+        //constructor=>get executed whenever object created
+        public Student(String a, String b, String c, String d)
+        {
+            this.sname = a;
+            this.batchname = b;
+            this.dob = c;
+            this.gender = d;
+            
+        }
+
+
+        //This is where we display student details
+        public void display_details()
+        {
+            
+            Console.WriteLine("Student Details");
+            Console.WriteLine("=====================");
+            Console.WriteLine("Student Name = " + this.sname);
+            Console.WriteLine("Batch = " + this.batchname);
+            Console.WriteLine("Date of Birth = " + this.dob);
+            Console.WriteLine("Gender = " + this.gender);
+        }
+    }
+}
